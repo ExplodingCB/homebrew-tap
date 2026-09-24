9a27e02387f67cc9af445b9f4736ed53c1037ecf
ExplodingCB <explodingcommandblock@gmail.com>
2026-09-24 17:56:26 -0400

    tv-remote 1.0.0

--- Casks/tv-remote.rb
+++ Casks/tv-remote.rb
@@ -1,6 +1,6 @@
 cask "tv-remote" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "1.0.0"
+  sha256 "ce5b604d2c9fc04e16bd31827fff815bb9b04f2f0f8f89a1e7e44050d445b777"
 
   url "https://github.com/ExplodingCB/tv-remote/releases/download/v#{version}/TV-Remote-#{version}.zip"
   name "TV Remote"
